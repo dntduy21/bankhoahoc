05441aed5f9b586c31712e8155cd1f3ec1208334
Dinh Ngoc Tran Duy <dntduy.21@gmail.com>
2025-03-24 12:55:53 +0700

    upload img

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -11,40 +11,19 @@ import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        String uploadDir = Paths.get("uploads").toAbsolutePath().toUri().toString();
-//        registry.addResourceHandler("/uploads/**")
-//                .addResourceLocations(uploadDir);
-//    }
-
     @Value("${upload.path}")
     private String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:///C:/uploads/");
-
-        // Đường dẫn riêng cho thư mục /uploads/Image/
-        registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadPath + "/");
-        registry.addResourceHandler("/Image/**")
-                .addResourceLocations("file:///C:/uploads/");
-        registry.addResourceHandler("/Image/**")
-                .addResourceLocations("file:" + uploadPath + "/");
-
     }
 
     @PostConstruct
     public void initUploadDirs() {
         File uploadDir = new File(uploadPath);
-        File tempDir = new File(uploadPath + "temp");
-
         if (!uploadDir.exists()) uploadDir.mkdirs();
-        if (!tempDir.exists()) tempDir.mkdirs();
-
         System.out.println("Upload Path: " + uploadDir.getAbsolutePath());
-        System.out.println("Temp Path: " + tempDir.getAbsolutePath());
     }
 }
